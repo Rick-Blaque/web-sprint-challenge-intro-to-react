3912edb6501b5cd25a4c3ee91ba6b8ac09afda6d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-07-31 01:16:38 -0400

    listed names and birth dates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+//import data from 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [friendsName, setFriendsName] = useState([]);
+  const fetchfriends = useEffect(() => {
+    axios.get('https://swapi.dev/api/people').then(function(data) {
+      setFriendsName(data.data)
+
+      })}, [])
+      
+  
+ 
+ //console.log(friendsName.name);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+   let names = ['e', 'r']
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character friends={friendsName} 
+      key={friendsName.name}
+      />
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from 'react';
+
+ const Character = bl => {
+     console.log(bl);
+    //   const [friends, key] = bl;
+    // const charter = [1,2,3,4]
+
+    return(
+        <div>
+            {bl.friends.map(c => {
+                return <h1>{c.name}            {c.birth_year}</h1>
+            })}
+        </div>
+    )
+};
+export default Character;
